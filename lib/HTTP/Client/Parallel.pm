718323639236a9bdd04135b968cbf4cd9f237c8e
Adam Kennedy <adamk@cpan.org>
2008-07-05 20:49:39 +0000

    Import of ADAMK/HTTP-Client-Parallel-0.02 from CPAN.
    gitpan-cpan-distribution: HTTP-Client-Parallel
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ADAMK/HTTP-Client-Parallel-0.02.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/HTTP/Client/Parallel.pm
+++ lib/HTTP/Client/Parallel.pm
@@ -66,16 +66,20 @@ programming can still take advantage of the capabilities of POE and
 gain major speed increases in HTTP-based programs with relatively
 little work.
 
+=head1 METHODS
+
+TO BE COMPLETED
+
 =cut
 
 use 5.006;
 use strict;
 use warnings;
-use Exporter     ();
-use IO::File     ();
-use Params::Util '_INSTANCE';
-use HTTP::Date   ();
-use HTTP::Request;
+use Exporter      ();
+use IO::File      ();
+use Params::Util  '_INSTANCE';
+use HTTP::Date    ();
+use HTTP::Request ();
 use POE;
 use POE::Session;
 use POE::Component::Client::HTTP;
@@ -86,14 +90,14 @@ use constant DEFAULT_TIMEOUT        => 60;
 
 use vars qw{$VERSION @ISA @EXPORT_OK};
 BEGIN {
-	$VERSION   = '0.01';
-	@ISA       = 'Exporter';
-	@EXPORT_OK = qw{ mirror getstore get };
+    $VERSION   = '0.02';
+    @ISA       = 'Exporter';
+    @EXPORT_OK = qw{ mirror getstore get };
 }
 
 sub new {
     my ( $class, %args )  = @_;
-    return  bless {
+    return bless {
         requests       => {},
         results        => {},
         count          => 0,
@@ -108,29 +112,55 @@ sub urls {
     return wantarray ? @{ $_[0]->{urls} } : $_[0]->{urls};
 }
 
-sub _set_urls {
-    my ( $self, @urls ) = @_;
-    $self->{urls} = \@urls;
-    $self->{url_count} = @urls;
-    return;
-}
-
 sub get {
     my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
+    $self->_init;
     $self->_set_urls(@_);
     $self->poe_loop;
     my @responses = map { $self->{responses}{$_} } $self->urls;   
     return wantarray ? @responses : \@responses
+
+    # XXX from the pod, this may be the desired behavior
+    # my @content = map { $self->{responses}{$_}->content } $self->urls;
+    # return wantarray ? @content : \@content
 }
 
 sub getstore {
     my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
     my %url_file_map = @_;
+    $self->_init;
     $self->_set_urls( keys %url_file_map );
     $self->{local_files} = \%url_file_map;
     $self->poe_loop;
     my @responses = values %{ $self->{responses} };
     return wantarray ? @responses : \@responses;
+}
+
+sub mirror {
+    my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
+    my %url_file_map = @_;
+    $self->_init;
+    $self->_set_urls( keys %url_file_map );
+    $self->{local_files} = \%url_file_map;
+    $self->_build_modified_since( \%url_file_map );
+    $self->poe_loop;
+    my @responses = values %{ $self->{responses} };
+    return wantarray ? @responses : \@responses
+}
+
+# cleanup between uses just in case
+sub _init {
+    my ($self) = @_;
+    $self->{count} = 0;
+    delete @{$self}{qw(requests responses urls url_count local_files modified_since)};
+    return;
+}
+
+sub _set_urls {
+    my ( $self, @urls ) = @_;
+    $self->{urls} = \@urls;
+    $self->{url_count} = @urls;
+    return;
 }
 
 sub _build_modified_since {
@@ -144,17 +174,6 @@ sub _build_modified_since {
     }
 }
 
-sub mirror {
-    my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
-    my %url_file_map = @_;
-    $self->_set_urls( keys %url_file_map );
-    $self->{local_files} = \%url_file_map;
-    $self->_build_modified_since( \%url_file_map );
-    $self->poe_loop;
-    my @responses = values %{ $self->{responses} };
-    return wantarray ? @responses : \@responses
-}
-
 sub _store_local_file {
     my ( $self, $response, $file ) = @_;
 
@@ -164,6 +183,7 @@ sub _store_local_file {
     print $tmp_fh $response->content;
     close $tmp_fh;
 
+    # the following is taken from LWP::UserAgent->mirror
     my $file_length = ( stat($tmpfile) )[7];
     my ($content_length) = $response->header('Content-length');
 
@@ -193,8 +213,6 @@ sub _store_local_file {
 # POE 
 sub poe_loop {
     my $self = shift;
-
-    $self->{count} = 0;
 
     POE::Component::Client::HTTP->spawn(
         Alias           => $self->{http_alias} || 'ua',
@@ -267,7 +285,8 @@ sub _response {
         warn '[' . $request->uri . "] Not Modified\n" if $self->{debug};
     }
     else {
-        warn '[' . $request->uri . "] HTTP Response Code: " . $response->code . "\n" if $self->{debug};
+        warn '[' . $request->uri . "] HTTP Response Code: " . $response->code . "\n"
+            if $self->{debug};
     }
 
     $kernel->call( ua => 'shutdown' ) if $self->{url_count} == $self->{count};
@@ -299,11 +318,11 @@ For other issues, contact the author.
 
 =head1 AUTHORS
 
-Jeff Bisbee E<lt>jbisbee@cpan.orgE<gt>
+Marlon Bailey E<lt>mbailey@cpan.orgE<gt>
 
 Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
-Marlon Bailey E<lt>mbaily@cpan.orgE<gt>
+Jeff Bisbee E<lt>jbisbee@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
@@ -311,7 +330,7 @@ L<LWP::Simple>, L<POE>
 
 =head1 COPYRIGHT
 
-Copyright 2008 Jeff Bisbee, Adam Kennedy and Marlon Bailey.
+Copyright 2008 Marlon Bailey, Adam Kennedy and Jess Bisbee.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
